32fcd8b23b796c9f9b684700a8cf0f04e2064001
Aleks Bozhinov <abozhino@visteon.com>
2022-08-28 17:02:30 +0300

    ArraysDefines.h repaired

--- Arrays/headers/ArraysDefines.h
+++ Arrays/headers/ArraysDefines.h
@@ -1,10 +1,18 @@
+//------------------------------------------------------
+// NAME: Aleks Bozhinov
+// PROBLEM: Arrays implementation
+// FILE NAME: ArraysDefines.h
+// FILE PURPOSE:
+// Declarations of struct Array helper structs and/or enums
+//------------------------------------------------------
+
 #ifndef ARRAYS_DEFINES_H_
 #define ARRAYS_DEFINES_H_
 
-enum SortingDirection
+typedef enum _SortingDirection
 {
-    ASCENDING = 1,
-    DESCENDING = -1
-};
+    ASCENDING  = 1,
+    DESCENDING = -1,
+}SortingDirection;
 
 #endif // ARRAYS_DEFINES_H_
